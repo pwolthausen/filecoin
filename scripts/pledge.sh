cf5f954c74b99ea2febe9a31aa338c538a8e43b5
Patrick Wolthausen <50278685+pwolthausen@users.noreply.github.com>
2021-09-24 14:50:46 -0400

    Create pledge.sh

---
+++ scripts/pledge.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+if [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11]
+then
+	while [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11]
+	do
+		/usr/local/bin/lotus-miner sectors pledge
+	done
+fi
