8a78e7eabdec3c8d03d27d407109e515dc4d0a8c
Patrick W <pwolthausen@cloudops.com>
2021-09-25 11:02:04 -0400

    corrected bash syntax

--- scripts/pledge.sh
+++ scripts/pledge.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-if [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11]
+if [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11 ]
 then
-	while [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11]
+	while [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11 ]
 	do
 		/usr/local/bin/lotus-miner sectors pledge
 	done
