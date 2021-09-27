8878150f9d1437e0ed08255f9a64cc07380b1d02
Patrick W <pwolthausen@cloudops.com>
2021-09-26 22:43:36 -0400

    update scripts, add backups

--- scripts/pledge.sh
+++ scripts/pledge.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 
-if [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11 ]
+i=0
+date +%H:%M
+if [ $(/usr/local/bin/lotus-miner sectors list | grep -v Proving | wc -l) -lt 7 ]
 then
-	while [ $(/usr/local/bin/lotus-miner sealing jobs | wc -l) -lt 11 ]
+	echo "needs more pledges"
+	while [ $(/usr/local/bin/lotus-miner sectors list | grep -v Proving | wc -l) -lt 7 ]
 	do
 		/usr/local/bin/lotus-miner sectors pledge
+		echo "pledge added"
+		((i++))
+		if [[ "$i" == '6' ]]
+	       	then
+			break
+		fi
 	done
+else
+	echo "no new pledges needed"
 fi
