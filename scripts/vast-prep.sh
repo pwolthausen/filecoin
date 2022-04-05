e792fc6c92abe4805a125cd849b31847131394d0
Patrick W <pwolthausen@cloudops.com>
2022-04-05 10:15:51 -0400

    changed order of install

--- scripts/vast-prep.sh
+++ scripts/vast-prep.sh
@@ -3,9 +3,16 @@
 PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDXGmP6L/zc9HsEKymjhSQS7mWjagPk7QVz25dcTNVOuAlMXewWyC7HpYmH+1Q9JvJcXwK3xpQYqzwBSNAUrQYHsRYl9K20b0vbmnZZWNsaVQNy+p5rcVhYCzqJZf0bXk95T6SR/51WAS8vJSYeJtbCYRTt5ar25y5KQ+O2PiBGC+77XJ+nbFaXJvSVjnR2RPzH1XhnOkZyWlcYVeLBk78kpwqkq5fX175pnvCYnXyCAhRL1Cn09a9OPQSCxpaxcgDdTsfZx45l9tOeN+5Zl8WYKmPZ1gC47KLTXN5T7W0LR6rOZlY4xOjQ+jiM/0cNeiTFkeS2G8+0nqK7aat8SXovM25FJPQNKj59ZAI++MguBsLn+4mreoEp8sA8GqwMbunFFtTPGSkBb16wj+GrOcJ8noPkEH96yB73FUrCxHNHC9/AToj9mutqqIiwc/8eBzlkyGUSKYs+j5JpPvpkAj6UCNclwbD6jkFGNKEqvbjCYlFMPtIVTwsml5GcICFMgg8= pwolthausen@duncan"
 
 apt update -y && apt upgrade -y
-apt remove nvidia*
-apt autoremove
-apt install -y nvidia-driver-510 python2 openssh-server
+apt install -y python2 openssh-server vim
+
+## Install Nvidia drivers
+apt-get purge -y '*nvidia*'
+apt-get purge -y '*libnvidia*'
+apt-get purge -y '*cuda*'
+apt autoremove -y
+add-apt-repository ppa:graphics-drivers/ppa --yes
+apt update
+apt install -y nvidia-driver-510
 
 ## Configure Open SSH
 echo "Install and configure open SSH"
@@ -35,7 +42,7 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
       && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add - \
       && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-apt-get update -y
+apt-get update
 apt-get install -y nvidia-docker2
 systemctl restart docker
 
@@ -48,4 +55,7 @@ echo "%vastAdmins ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/vastAdm
 mkdir /home/pwolthausen/.ssh && chmod 700 /home/pwolthausen/.ssh && chown pwolthausen /home/pwolthausen/.ssh
 echo $PUBLIC_KEY > /home/pwolthausen/.ssh/authorized_keys && chmod 600 /home/pwolthausen/.ssh/authorized_keys && chown pwolthausen /home/pwolthausen/.ssh/authorized_keys
 
+## Clean up
+apt update && apt upgrade -y
 apt autoremove -y
+reboot now
